77bba66c9f8bda2938bddf155468aba225cb9927
Ab0ba101 <timohapaskevic101@gmail.com>
2023-07-17 20:19:52 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1
+let Result = 5+5-"5";
+console.log(Result);
+console.log(typeof Result);
+
+// 2
+let Email = "timohapaskevic101@gmail.com";
+console.log("@ There is:", Email.includes("@"));
+console.log(Email.length);
+
+// 3
+let My = "My";
+let name = "name";
+let is = "is";
+let fullName = `${My} ${name} ${is} Viktor`;
+console.log(fullName);
+
+// 4
+let userName = "Тимофій";
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
